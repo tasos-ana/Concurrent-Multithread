74498149e2e02afd815d82e902b9a5010d9abcf2
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-04-28 00:13:39 +0300

    [FIX]
    -use flag to pass input from main

--- HY486_PhaseA/src/main.c
+++ HY486_PhaseA/src/main.c
@@ -6,29 +6,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-#include "client.h"
-#include "updater.h"
+//#include "client.h"
+//#include "updater.h"
+void usage();
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
-        fprintf(stderr, "Wrong arguments! Usage: [program name] [client Number] [updater Number] [input file path]\n");
-        return (EXIT_FAILURE);
+    int opt;
+    int clients = 0;
+    int updaters = 0;
+    char *file = NULL;
+
+    /* A very easy way to parse command line arguments */
+    while ((opt = getopt(argc, argv, "c:u:f:")) != -1) {
+        switch (opt) {
+            case 'c':
+                clients = atoi(optarg);
+                break;
+            case 'u':
+                updaters = atoi(optarg);
+                break;
+            case 'f':
+                file = strdup(optarg);
+                break;
+            default:
+                usage();
+                exit(EXIT_FAILURE);
+        }
+    }
+    if (clients > 0 && updaters > 0 && file != NULL) {
+        printf("updater: %d \n client: %d\n file: %s\n", updaters, clients, file);
+        free(file);
     } else {
-        int clients = atoi(argv[1]);
-        int updaters = atoi(argv[2]);
-        char* file = argv[3];
-        if (clients < 0) {
-            fprintf(stderr, "Client number cant be negative\n");
-            return (EXIT_FAILURE);
-        }
-        if (updaters < 0) {
-            fprintf(stderr, "Updater number cant be negative\n");
-            return (EXIT_FAILURE);
-        }
-        initClients(clients, file);
-        initUpdaters(updaters, file);
+        usage();
+        return EXIT_FAILURE;
     }
     return (EXIT_SUCCESS);
+}
+
+void usage() {
+    printf("Usage: prog -c clientNum -u updaterNum -f file"
+            "Options:\n"
+            "   -c <int>            Set the clients thread number.\n"
+            "   -u <int>            Set the updater thread number.\n"
+            "   -f <string>         Set -f option specifies the file path of the input.\n"
+            "   -h                  prints this help\n");
 }
 
